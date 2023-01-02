1979ab6f989090065cdba5776c825db2f4bc45d9
Vladimir83 <vovaf@bk.ru>
2023-01-02 19:14:26 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ряд числе через пробел");
+var str = Console.ReadLine() ?? "";
+
+int[] arr = Array.ConvertAll(str.Split(' '), int.Parse);
+
+int posSum = 0;
+for (int i = 0; i < arr.Length; i++)
+    if (arr[i] > 0) posSum++;
+
+Console.WriteLine($"Количество чисел больше 0 равно: {posSum}");
