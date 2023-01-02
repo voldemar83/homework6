85dd37988d676841f77aab569554dadd64efb111
Vladimir83 <vovaf@bk.ru>
2023-01-02 19:17:02 +0300

    task43

---
+++ task43/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetValue(string varname)
+{
+    Console.Write($"Введите значение {varname}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int b1 = GetValue("b1");
+int k1 = GetValue("k1");
+int b2 = GetValue("b2");
+int k2 = GetValue("k2");
+
+double x = (double)(b1 - b2) / (k1 - k2) * -1;
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых имеет координаты: ({x}; {y})");
